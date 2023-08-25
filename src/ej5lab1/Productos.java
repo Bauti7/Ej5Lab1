5fc0597258de918c017d2c8f49be245e1ac26c67
Bauti7 <juanbau2000@hotmail.es>
2023-08-24 21:06:17 -0300

    Initial commit

---
+++ src/ej5lab1/Productos.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej5lab1;
+
+/**
+ *
+ * @author Bau
+ */
+public class Productos {
+  
+    private String nombre;
+    private int precio;
+    private String categoria;
+
+    public Productos(String nombre, int precio, String categoria) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+    
+    
+}
+    
+
